18beae7d3ab8c7c066eee76eab19a88bc77d2616
Peter Dye <pjad98@gmail.com>
2021-06-26 14:38:03 -0400

    drawing a basic background on the table

--- public/table.js
+++ public/table.js
@@ -8,17 +8,32 @@ class Table extends React.Component {
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleMove = this.handleMove.bind(this);
+
+    this.state = {
+      width: 300,
+      height: 150
+    }
   }
 
   render() {
     return (
       <canvas
-        id='Table'
+        ref='table'
         tabIndex='1'
         onKeyPress={this.handleKeyPress}
+        width={this.state.width}
+        height={this.state.height}
       >
       </canvas>
     );
+  }
+
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
   }
 
   handleKeyPress(e) {
@@ -39,5 +54,33 @@ class Table extends React.Component {
   handleMove(message) {
     var message = JSON.parse(message);
     console.log(message);
+  }
+
+  updateCanvas() {
+    const ctx = this.refs.table.getContext('2d');
+    this.drawBackground(ctx);
+  }
+
+  drawBackground(ctx) {
+    var width = this.state.width;
+    var height = this.state.height;
+
+    ctx.fillStyle = 'rgb(0, 0, 200)';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = '10';
+    ctx.strokeRect(0, 0, width, height);
+
+    ctx.lineWidth = '5';
+    ctx.beginPath();
+    ctx.moveTo(0, height/2);
+    ctx.lineTo(width, height/2);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(width/2, 0);
+    ctx.lineTo(width/2, height);
+    ctx.stroke();
   }
 }
